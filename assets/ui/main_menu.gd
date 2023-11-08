9a0046ccfcdbe2823b12c51a9b8914a6ebac8472
MrJackphil <geimerus@gmail.com>
2023-11-08 19:33:37 +0400

    chore: :wrench: load one scene on start_game

--- assets/ui/main_menu.gd
+++ assets/ui/main_menu.gd
@@ -4,14 +4,14 @@ extends Node
 @export var options_btn: Button
 @export var exit_game_btn: Button
 
-@export var scenes: Array[PackedScene]
+@export var scene_to_load: PackedScene = preload("res://Test_Scene.tscn")
 
 func _ready():
 	start_game_btn.pressed.connect(_start_game)
 	exit_game_btn.pressed.connect(_exit_game)
 
 func _start_game():
-	get_tree().change_scene_to_packed(scenes[0])
+	get_tree().change_scene_to_packed(scene_to_load)
 
 func _exit_game():
 	get_tree().quit()
